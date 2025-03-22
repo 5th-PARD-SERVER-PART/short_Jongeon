603789975b4cf4c9b528636422297fae499f4287
chianddo223 <whddjs0210@naver.com>
2025-03-22 14:17:28 +0900

    eoeo

---
+++ week3/src/test/java/com/pard/server/week3/Week3ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pard.server.week3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//eqweqw
+	}
+
+}
